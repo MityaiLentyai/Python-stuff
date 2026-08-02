c0033c606126fe77a62268629b5dee621cbafd7d
Mitya Zayats <luckmanpackman@gmail.com>
2026-08-03 00:08:40 +0200

    First commit

---
+++ ex1/ft_garden_name.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+def	ft_garden_name():
+	name = (input("Enter garden name : "))
+	print("Garden  \nStatus: Growing well!")
+
+ft_garden_name()
---
+++ ex2/ft_plot_area.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+def	ft_plot_area():
+	print("Enter length: ", end="")
+	legnth = int(input())
+	print("Enter width: ", end="")
+	width = int(input())
+	print("Plot area:", legnth*width)
+
+ft_plot_area()
