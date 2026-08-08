570927b064e0b896fe45c0614295442eb21e3fbe
Mitya Zayats <luckmanpackman@gmail.com>
2026-08-08 23:23:15 +0200

    A fucking cleanup

--- ex0/ft_hello_garden.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env python3
-
-def ft_hello_garden():
-    print("Hello, Garden Community!")
--- ex1/ft_garden_name.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python3
-
-def ft_garden_name():
-    name = (input("Enter garden name : "))
-    print(f"Garden: {name} \nStatus: Growing well!")
