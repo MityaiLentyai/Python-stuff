0d27d2f08338d673e74b6b3ebebc196c74f05105
Mitya Zayats <luckmanpackman@gmail.com>
2026-08-09 15:56:33 +0200

    MyPy fixes + restructuring

---
+++ Python_Module_1/ex1/ft_garden_data.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+class Plant:
+
+    all_plants: list["Plant"] = []
+
+    def __init__(self, name,  height,  age):
+        self.name = name
+        self.height = height
+        self.age = age
+
+        Plant.all_plants.append(self)
+
+    @classmethod
+    def show(cls):
+        print("=== Garden Plant Registry ===")
+        if not Plant.all_plants:
+            print("No plants received yet")
+            return
+
+        for plant in cls.all_plants:
+            print(f"{plant.name.capitalize()}: {plant.height}cm, "
+                  f"{plant.age} days old")
+
+
+def main():
+
+    Plant("rose", 25, 30)
+    Plant("sunflower", 80, 45)
+    Plant("cactus", 15, 120)
+    Plant.show()
+
+
+if __name__ == "__main__":
+    main()
