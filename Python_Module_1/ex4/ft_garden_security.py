b7591e0df6eccdfd0a6f461d5141150545db8ff3
Dzmitry Zayats <dzzayats@student.42warsaw.pl>
2026-08-17 23:45:10 +0200

    WTF

---
+++ Python_Module_1/ex4/ft_garden_security.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+class Plant:
+
+    def __init__(self, name: str, height: float, age_days: int):
+        self.name: str = name
+        self.height: float = height
+        self.age_days: int = age_days
+
+    def show(self) -> None:
+        print(f"{self.name}: height={self.height}, age_days={self.age_days}")
+
+    def set_height(self) -> None:
+        self.height = 
+    def set_age() -> None:
+
+
+def main() -> None:
+    print("=== Garden Security System ===")
+
+
+if __name__ == "__main__":
+    main()
