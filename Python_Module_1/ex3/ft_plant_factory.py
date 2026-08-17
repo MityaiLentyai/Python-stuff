38a2598a67ce18ec770dcc184c848a2c8e18181c
Mitya Zayats <luckmanpackman@gmail.com>
2026-08-17 17:17:59 +0200

    Whatever, not sure

--- Python_Module_1/ex3/ft_plant_factory.py
+++ Python_Module_1/ex3/ft_plant_factory.py
@@ -2,35 +2,26 @@
 
 class Plant:
 
-    def __init__(self, name: str,  height: float,  age_days: int):
-        self.name: str = name
-        self.height: float = height
-        self.age_days: int = age_days
+    def __new___(cls, name: str,  height: float,  age_days: int):
+        class.name: str = name
+        class.height: float = height
+        class.age_days: int = age_days
 
-    def age(self):
-        self.age_days += 1
-
-    def grow(self):
-        self.height = round(self.height * 1.01, 1)
-    def show(self):
-        if not self:
-            print("No plants received yet")
-            return
-
-        print(f"{self.name.capitalize()}: {self.height}cm, "
-              f"{self.age_days} days old")
-
+    def __init__()
 
 def main() -> None:
     print("=== Plant Factory Output ===")
     garden: list[Plant] = [
         Plant("Rose", 25.0, 30),
-        Plant("Bamboo", 50.0, 10),
-        Plant("Cactus", 10.0, 100)
+        Plant("Bamboo", 80.0, 10),
+        Plant("Cactus", 10.0, 100),
+        Plant("Sunflower", 40.0, 45),
+        Plant("Cactus", 15.0, 120)
     ]
 
     for plant in garden:
         plant.show()
+
 
 if __name__ == "__main__":
     main()
