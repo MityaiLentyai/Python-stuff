f451861533ad520614747f51fe51f1a281c6b1e4
Dzmitry Zayats <dzzayats@student.42warsaw.pl>
2026-08-17 20:22:09 +0200

    ex4 start

--- Python_Module_1/ex3/ft_plant_factory.py
+++ Python_Module_1/ex3/ft_plant_factory.py
@@ -4,8 +4,22 @@ class Plant:
 
     def __init__(self, name: str, height: float, age_days: int):
         self.name: str = name
+        if not self.name:
+            print("No plants received yet")
+            return
         self.height: float = height
+        if height <= 0:
+            print("Height can not be zero or negative")
         self.age_days: int = age_days
+        if age_days <= 0:
+            print("Age can not be zero or negative")
+
+    def set_height(self, new_hight: int):
+        if self.age < 0:
+            print(f"{self.name}: Error, can't be negative")
+        else:
+            self.height = new_hight
+            print(f"Height updated: {}")
 
     def show(self) -> None:
         print(f"{self.name}: height={self.height}, age_days={self.age_days}")
@@ -13,16 +27,10 @@ class Plant:
 
 def main() -> None:
     print("=== Plant Factory Output ===")
-    garden: list[Plant] = [
-        Plant("Rose", 25.0, 30),
-        Plant("Bamboo", 80.0, 10),
-        Plant("Cactus", 10.0, 100),
-        Plant("Sunflower", 40.0, 45),
-        Plant("Cactus", 15.0, 120)
-    ]
 
-    for plant in garden:
-        plant.show()
+    rose = Plant("Rose", 15.0, 30)
+    print("Plant created: ", end="")
+    rose.show()
 
 
 if __name__ == "__main__":
