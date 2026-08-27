f2ae529b4beb88d09a03e504ab20f2ba192f5342
Dzmitry Zayats <dzzayats@student.42warsaw.pl>
2026-08-27 02:31:51 +0200

    ex1 solved, ex2 started

--- Python_Module_3/ex1/ft_score_analytics.py
+++ Python_Module_3/ex1/ft_score_analytics.py
@@ -1,26 +1,67 @@
+# import sys
+
+
+# def script() -> None:
+
+#     scores = []
+#     print("=== Player Score Analytics ===")
+#     for x in sys.argv[1:]:
+#         try:
+#             scores.append(int(x))
+#         except Exception:
+#             print(f"Invalid parameter: '{x}'")
+#             continue
+#     if len(scores) == 0:
+#         print("No scores provided.", end="")
+#         print("Usage: python3 ft_score_analytics.py <score1> <score2> ...")
+#         return
+#     try:
+#         print(f"Scores processed {scores}")
+#         print(f"Total score: {sum(scores)}")
+#         print(f"Total players: {len(scores)}")
+#         print(f"Total score: {sum(scores)}")
+#         print(f"Average score: {(sum(scores))/(len(scores))}")
+#         print(f"High score: {max(scores)}")
+#         print(f"Low score: {min(scores)}")
+#         print(f"Score range: {max(scores)-min(scores)}")
+#     except Exception:
+#         print("SOME FUCKING ERROR AGAIN")
+
+
+# script()
+
 import sys
 
-# class No_scores(Exception):
-#     def print("No scores provided. Usage: python3 ft_score_analytics.py <score1> <score2> ...
-# ")
+
+def try_int(val: str):
+    try:
+        return int(val)
+    except Exception:
+        print(f"Invalid parameter: '{val}'")
+        return None
 
 
 def script() -> None:
     print("=== Player Score Analytics ===")
-    scores = [None] * len(sys.argv)
-    if len(sys.argv) == 1:
-        print("No scores provided. Usage: python3 ", end="")
-        print("ft_score_analytics.py <score1> <score2> ...")
-    else:
-        print(f"Scores processed {sys.argv[1:]}")
+    processed = [try_int(x) for x in sys.argv[1:]]
+    scores = [score for score in processed if score is not None]
+
+    if len(scores) == 0:
+        print("No scores provided.", end="")
+        print("Usage: python3 ft_score_analytics.py <score1> <score2> ...")
+        return
+
+    try:
+        print(f"Scores processed {scores}")
         print(f"Total score: {sum(scores)}")
-
-    # i: int = 1
-    # while i < len(sys.argv):
-    #     print(f"Argument {i}: {sys.argv[i]}")
-    #     i += 1
-
-    print(f"Total players: {len(sys.argv)-1}")
+        print(f"Total players: {len(scores)}")
+        print(f"Total score: {sum(scores)}")
+        print(f"Average score: {(sum(scores))/(len(scores))}")
+        print(f"High score: {max(scores)}")
+        print(f"Low score: {min(scores)}")
+        print(f"Score range: {max(scores)-min(scores)}")
+    except Exception:
+        print("SOME FUCKING ERROR AGAIN")
 
 
 script()
