bb95987e8d4f87a3d10d594c82b9e0a81d7ff348
Dzmitry Zayats <dzzayats@student.42warsaw.pl>
2026-08-28 16:10:52 +0200

    Changes + module 0 restore???

--- Python_Module_3/ex2/ft_coordinate_system.py
+++ Python_Module_3/ex2/ft_coordinate_system.py
@@ -5,18 +5,26 @@ def game():
     print("=== Game Coordinate System ===\n")
     print("Get a first set of coordinates")
 
-    while(True):
+    while True:
         # fmt: off
         try:
             user_input = input(
                 "Enter new coordinates Enter new"
                 "coordinates as floats in format 'x,y,z': "
             ).split(',')
-            coords = tuple(float(value) for value in user_input)
-            print(coords)
+            x, y, z = tuple(float(value) for value in user_input)
+            print(x, y, z)
+            print("It includes: ", end="")
+            print(f"X={x}", )
+            print(f"Y={y}")
+            print(f"Z={z}")
+            return x, y, z
+
+            break
         except Exception:
             print("Invalid syntax")
         # fmt: on
+
 
 
 game()
