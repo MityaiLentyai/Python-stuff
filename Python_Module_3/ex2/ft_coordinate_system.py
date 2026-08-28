a727717539611f5d2f014c75bd4aecfa269bf20f
Mitya Zayats <luckmanpackman@gmail.com>
2026-08-28 14:37:28 +0200

    ex2 stuff

--- Python_Module_3/ex2/ft_coordinate_system.py
+++ Python_Module_3/ex2/ft_coordinate_system.py
@@ -5,16 +5,18 @@ def game():
     print("=== Game Coordinate System ===\n")
     print("Get a first set of coordinates")
 
-    # fmt: off
-    try:
-        coords = int(input(
-            "Enter new coordinates Enter new"
-            "coordinates as floats in format 'x,y,z': "
-        ))
-    except Exception:
-        print("Invalid syntax")
-    
-    # fmt: on
+    while(True):
+        # fmt: off
+        try:
+            user_input = input(
+                "Enter new coordinates Enter new"
+                "coordinates as floats in format 'x,y,z': "
+            ).split(',')
+            coords = tuple(float(value) for value in user_input)
+            print(coords)
+        except Exception:
+            print("Invalid syntax")
+        # fmt: on
 
 
 game()
