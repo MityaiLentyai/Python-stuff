d82f4c4cb6af1cab99df491b6b38b65302de8d84
Dzmitry Zayats <dzzayats@student.42warsaw.pl>
2026-08-28 22:48:51 +0200

    Ex3

---
+++ Python_Module_3/ex3/ft_achievement_tracker.py
@@ -0,0 +1,54 @@
+import random
+
+
+def gen_player_achivements():
+    player_names = ["Emma", "Liam", "Sophia", "Ethan"]
+    achivements = [
+        "Monster Hunter",
+        "Dragon Born",
+        "Mythic Legend",
+        "Shadow Ninja",
+        "Gold Digger",
+        "Lore Master",
+        "Speed Demon",
+        "Survival Expert",
+        "Iron Wall",
+        "Sharpshooter",
+        "Ghost Walker",
+        "Apex Predator",
+        "Gladiator",
+        "Hoarder",
+        "Puzzle Solver",
+        "First Blood",
+        "Immortal",
+        "Architect",
+        "Rich Scholar",
+        "Beast Tamer",
+    ]
+    players = []
+    for name in player_names:
+        count = random.randint(0, 20)
+        random_achivements = random.sample(achivements, count)
+        achivements_set = set(random_achivements)
+        players.append(achivements_set)
+        print(f"Player {name}: {achivements_set}")
+    all_distinct = set.union(*players)  # star unpacks the list to avoid a loop
+    print(f"\nAll distinct achievements: {all_distinct}\n")
+
+    all_common = set.intersection(*players)
+    print(f"Commom achivements: {all_common}\n")
+
+    for name, current_set in zip(player_names, players):
+        # It seems that I overcomplicated this shit, but alas
+        other_sets = [p_set for p_set in players if p_set is not current_set]
+        unique_to_player = current_set.difference(*other_sets)
+
+        print(f"Only {name} has: {unique_to_player}")
+
+    print()
+    for name, current_set in zip(player_names, players):
+        missing_achivements = all_distinct.difference(current_set)
+        print(f"{name} is missing: {missing_achivements}")
+
+
+gen_player_achivements()
