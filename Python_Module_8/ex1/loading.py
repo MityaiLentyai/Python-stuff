82bf86bb74799bcac84bde75a99848f69a125dcb
Mitya Zayats <luckmanpackman@gmail.com>
2026-09-21 02:04:20 +0200

    ex1 part for pip done

--- Python_Module_8/ex1/loading.py
+++ Python_Module_8/ex1/loading.py
@@ -1,17 +1,34 @@
-import importlib.util
+from importlib.util import find_spec
+from importlib.metadata import version
 
 if __name__ == "__main__":
-    modules = ["poetry", "pandas", "matplotlib", "numpy"]
-    if importlib.util.find_spec("poetry") is None:
-        print("poetry is not found to be installed\n"
-              "Install it with:\n"
-              "pip install poetry"
-              )
-    else:
-        for module in modules:
-            if importlib.util.find_spec(module) is None:
-                print(f"{module} is not found to be installed\n"
-                      f"Install it with:\n"
-                      f"pip install {module}\n"
-                      f"Or\n"
-                      f"poetry add {module}")
+
+    print("\nLOADING STATUS: Loading programs...\n")
+    print("Checking dependencies:")
+    modules = ["pandas", "numpy", "matplotlib"]
+    # if importlib.util.find_spec("poetry") is None:
+    #     print("poetry is not found to be installed\n"
+    #           "Install it with:\n"
+    #           "pip install poetry"
+    #           )
+    # else:
+    for module in modules:
+        if find_spec(module) is None:
+            print(f"{module} is not found to be installed\n"
+                  f"Install it with:\n"
+                  f"pip install {module}\n"
+                  f"Or\n"
+                  f"poetry add {module}")
+        elif find_spec(module) is not None:
+            print(f"[OK] {module} ({version(module)})", end="")
+            match module:
+                case "pandas":
+                    print(" - Data manipulation ready")
+                case "numpy":
+                    print(" - Numerical computation ready")
+                case "requests":
+                    print(" - Network access ready")
+                case "matplotlib":
+                    print(" - Visualization ready")
+                case _:
+                    continue
